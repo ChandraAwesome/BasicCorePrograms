1f1fc467b53c184fc84672a490317cda710dc41d
ChandarAwesome <ajaygames00Gmail.com>
2023-01-11 19:47:17 +0530

    [Chandar]-LeapYear

---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class LeapYear
+    {
+        
+            public static void Leap()
+            {
+                Random random = new Random();
+                int year = random.Next(1000, 10000);
+                if ((year % 400 == 0) || (year % 4 == 0))
+                {
+                    Console.WriteLine("It is a leap year :" + year);
+                }
+                else if (year % 100 != 0)
+                {
+                    Console.WriteLine("It is not a leap year :" + year);
+                }
+            }
+        
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Basic Core Programs");
+            while (true)
+            {
+                Console.WriteLine("Enter the option : 1) EvenorOdd \n 2) LeapYear ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        EvenorOdd.evenorodd();
+                        break;
+                    case 2:
+                        LeapYear.Leap();
+                        break;
+                }
+            }
         }
     }
 }
